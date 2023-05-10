55a2ed30936e0c8d60e49437da5733bebd0ced65
Vitali Sinitski <vitali.sin777@gmail.com>
2023-05-10 17:58:31 +0300

    3.53 IT test application-test

--- src/test/java/com/vitali/integration/service/UserInformationServiceIT.java
+++ src/test/java/com/vitali/integration/service/UserInformationServiceIT.java
@@ -1,57 +1,47 @@
 package com.vitali.integration.service;
 
 import com.vitali.dto.user.UserReadDto;
-
+import com.vitali.dto.userInformation.UserInformationReadDto;
 import com.vitali.integration.annotation.IT;
+import com.vitali.services.UserInformationService;
 import com.vitali.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.junit.Assert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-//import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestConstructor;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
-import static org.junit.Assert.assertNotNull;
-import static org.assertj.core.api.Assertions.*;
-import static com.vitali.util.TestConstants.USER_ID_ONE;
-import static com.vitali.util.TestConstants.USER_READ_DTO;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @IT
 @RequiredArgsConstructor
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-public class UserServiceIT {
-    private final UserService userService;
+public class UserInformationServiceIT {
+    private final UserInformationService userInformationService;
 
 
     @Test
     public void testFindAll() {
-
-        // when
-        List<UserReadDto> users = userService.findAll();
-
-        // then
-        assertThat(users).isNotNull();
-        assertThat(users).hasSize(12);
+//
+//        // when
+//        List<UserReadDto> users = userInformationService.findAll();
+//
+//        // then
+//        assertThat(users).isNotNull();
+//        assertThat(users).hasSize(12);
     }
 
     @Test
-    public void testFindById() {
+    public void findUserInformationByUserId() {
 
         // when
-        UserReadDto user = userService.findById(1);
+        UserInformationReadDto userInformationReadDto = userInformationService.findUserInformationByUserId(1);
 
         // then
-        assertThat(user).isNotNull();
-        assertThat(user.getUsername()).isEqualTo("Jack");
+        assertThat(userInformationReadDto).isNotNull();
+        assertThat(userInformationReadDto.getFirstName()).isEqualTo("Jack");
     }
 
 
